66cdf4c4d28a4c21e5f46f4394bfb9a29a7be90b
Ribbit <echo07200503@gmail.com>
2021-09-01 13:42:39 +0900

    値が配列になるアプリフィールドも検索するよう修正

--- src/desktop/states/filterd-records.ts
+++ src/desktop/states/filterd-records.ts
@@ -10,18 +10,31 @@ export const filterdRecordsState = selector<Record[]>({
     const input = get(searchTextState);
 
     return records.filter((record) =>
-      Object.values(record).some(({ value }) => {
-        if (!Array.isArray(value)) {
-          if (!value) {
-            return false;
-          } else if (typeof value === 'string') {
-            return ~value.indexOf(input);
-          } else {
-            return ~value.name.indexOf(input);
-          }
-        } else {
-          value;
-          return false; // 一旦非対応
+      Object.values(record).some((field) => {
+        switch (field.type) {
+          case 'CREATOR':
+          case 'MODIFIER':
+            return ~field.value.name.indexOf(input);
+
+          case 'CHECK_BOX':
+          case 'MULTI_SELECT':
+          case 'CATEGORY':
+            return field.value.some((value) => ~value.indexOf(input));
+
+          case 'USER_SELECT':
+          case 'ORGANIZATION_SELECT':
+          case 'GROUP_SELECT':
+          case 'STATUS_ASSIGNEE':
+            return field.value.some(({ name }) => ~name.indexOf(input));
+
+          case 'FILE':
+            return field.value.some(({ name }) => name.indexOf(input));
+
+          case 'SUBTABLE':
+            return false; // 非対応
+
+          default:
+            return field.value && ~field.value.indexOf(input);
         }
       })
     );
