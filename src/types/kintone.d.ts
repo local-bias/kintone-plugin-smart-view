24d41cc0d145e08918303ef3c3b5f3e121e50389
Ribbit <echo07200503@gmail.com>
2021-09-02 11:49:29 +0900

    プラグイン専用の型定義を分離

--- src/types/kintone.d.ts
+++ src/types/kintone.d.ts
@@ -1,20 +1,4 @@
 declare namespace kintone {
-  namespace plugin {
-    /**
-     * プラグインがアプリ単位で保存する設定情報
-     */
-    type Storage = {
-      conditions: Condition[];
-    };
-
-    type Condition = {
-      viewId: string;
-      viewDisplayingFields: string[];
-      enableCustomizedFiltering: boolean;
-      viewFilteringFields: string[];
-    };
-  }
-
   type EventType =
     | 'portal.show'
     | 'app.record.index.show'
---
+++ src/types/plugin.d.ts
@@ -0,0 +1,18 @@
+declare namespace kintone {
+  namespace plugin {
+    /**
+     * プラグインがアプリ単位で保存する設定情報
+     */
+    type Storage = {
+      conditions: Condition[];
+    };
+
+    type Condition = {
+      viewId: string;
+      viewDisplayingFields: string[];
+      enableCSVExport: boolean;
+      editable: boolean;
+      sortable: boolean;
+    };
+  }
+}
