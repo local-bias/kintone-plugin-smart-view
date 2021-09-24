28c28de4a0dbb9baf591710b2474a4b6934858be
Ribbit <echo07200503@gmail.com>
2021-09-24 11:31:21 +0900

    レコードが見つからなかった場合のユーザビリティ改善

--- src/desktop/components/table/empty.tsx
+++ src/desktop/components/table/empty.tsx
@@ -3,23 +3,30 @@ import styled from '@emotion/styled';
 
 const Component: VFCX = ({ className }) => (
   <div {...{ className }}>
-    <svg
-      role='img'
-      xmlns='http://www.w3.org/2000/svg'
-      width='92px'
-      height='92px'
-      viewBox='0 0 24 24'
-      stroke='#0004'
-      strokeWidth='0.5217391304347826'
-      strokeLinecap='square'
-      strokeLinejoin='miter'
-      fill='none'
-      color='#0004'
-    >
-      <path d='M3 10H9V13H15V10H21' />
-      <path d='M3 10L6 4H18L21 10V20H3V10Z' />
+    <svg height='200' viewBox='0 0 512 512' width='200'>
+      <g>
+        <path d='m256 149.554-20.001 131.791 20.001 230.655 198.791-65.896v-230.654z' fill='#bbb' />
+        <path d='m57.209 215.45v230.654l198.791 65.896v-362.446z' fill='#ccc' />
+        <g fill='#bbb'>
+          <path d='m241 0h30v100.174h-30z' />
+          <path
+            d='m122.957 31.63h30v100.174h-30z'
+            transform='matrix(.866 -.5 .5 .866 -22.376 79.926)'
+          />
+          <path
+            d='m323.957 66.717h100.174v30h-100.174z'
+            transform='matrix(.5 -.866 .866 .5 116.252 364.788)'
+          />
+        </g>
+        <path d='m198.791 372.217 57.209-90.872-198.791-65.895-57.209 90.872z' fill='#ddd' />
+        <path d='m313.209 372.217-57.209-90.872 198.791-65.895 57.209 90.872z' fill='#ccc' />
+      </g>
     </svg>
     <h2>条件に一致するレコードが見つかりませんでした。</h2>
+    <div>
+      <p>入力されている検索条件の他、一覧の絞り込み条件も適用されています。</p>
+      <p>お探しのレコードが見つからない場合は、絞り込み条件をご確認ください。</p>
+    </div>
   </div>
 );
 
@@ -32,14 +39,19 @@ const StyledComponent = styled(Component)`
   gap: 48px;
 
   h2 {
-    font-size: 24px;
+    font-size: 18px;
     font-weight: 600;
-    color: #0005;
+    color: #0007;
     padding: 0;
     margin: 0;
   }
+  p {
+    margin: 0;
+    color: #0005;
+  }
   svg {
     opacity: 0.4;
+    filter: drop-shadow(2px 2px 3px #0002);
     width: 200px;
     height: 200px;
   }
