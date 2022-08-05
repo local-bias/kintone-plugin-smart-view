a73c1f32c339fefc5372cf73d3f5875985686bc5
Ribbit <echo07200503@gmail.com>
2022-08-05 13:24:21 +0900

    ⚡ エラーログの収集頻度を制限

--- src/common/sentry.ts
+++ src/common/sentry.ts
@@ -1,12 +1,11 @@
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
 import { PLUGIN_NAME } from './statics';
 
 Sentry.init({
   dsn: 'https://947849a62ea4438b9b3fc3a04e41d2ae@o1300660.ingest.sentry.io/6535495',
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 1,
+  integrations: [],
+  tracesSampleRate: 0.3,
   initialScope: {
-    tags: { 'plugin-name': PLUGIN_NAME },
+    tags: { 'plugin-name': PLUGIN_NAME, host: window?.location?.host },
   },
 });
