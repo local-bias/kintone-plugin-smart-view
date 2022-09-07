5fd143ba3e4866a1b76ee71375d5901a8e11f20e
Ribbit <echo07200503@gmail.com>
2022-09-08 08:57:49 +0900

    ♻ 非公式APIの型定義を外部パッケージへ移行

--- src/common/cybozu.ts
+++ src/common/cybozu.ts
@@ -1,31 +1,3 @@
-const getSchema = () => cybozu.data.page.SCHEMA_DATA || cybozu.data.page.FORM_DATA.schema;
-
-export const getFields = () => {
-  const schema = getSchema();
-
-  return Object.values(schema.table.fieldList);
-};
-
-export const getSubtableFields = (subtableCode: string) => {
-  const schema = getSchema();
-
-  const subTables = Object.values(schema.subTable);
-
-  for (const subTable of subTables) {
-    if (subTable.var === subtableCode) {
-      return Object.values(subTable.fieldList);
-    }
-  }
-
-  return null;
-};
-
-export const getFieldMap = () => {
-  const fields = getFields();
-
-  return new Map(fields.map((field) => [field.var, field]));
-};
-
 export const getQueryString = () => {
   return cybozu?.data?.page?.QUERY_STRING ?? '';
 };
