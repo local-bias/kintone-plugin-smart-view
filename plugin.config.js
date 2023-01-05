2bd86657f5752dfbbe029c9410b6ce36726a7703
Ribbit <echo07200503@gmail.com>
2023-01-05 10:06:39 +0900

    🎉 2.14.0

--- plugin.config.js
+++ plugin.config.js
@@ -3,7 +3,7 @@ module.exports = {
   manifest: {
     base: {
       manifest_version: 1,
-      version: 2131,
+      version: '2.14.0',
       type: 'APP',
       name: {
         en: 'faster search plugin',
