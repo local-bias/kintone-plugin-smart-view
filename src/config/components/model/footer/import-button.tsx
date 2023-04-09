b49e2ce06b6141dac16e1eaf1071cf430a9167c8
Ribbit <echo07200503@gmail.com>
2023-04-09 17:07:08 +0900

    🐛 設定をインポートする際、文字コードを指定

--- src/config/components/model/footer/import-button.tsx
+++ src/config/components/model/footer/import-button.tsx
@@ -32,7 +32,7 @@ const Component: FC = () => {
             return;
           }
           const [file] = Array.from(files);
-          const fileEvent = await onFileLoad(file);
+          const fileEvent = await onFileLoad(file, 'UTF-8');
           const text = (fileEvent.target?.result ?? '') as string;
           set(storageState, JSON.parse(text));
           enqueueSnackbar('設定情報をインポートしました', { variant: 'success' });
