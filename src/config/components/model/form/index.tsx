307edd81e63bbd686c6f9217cec8a59939e3e0e8
Ribbit <echo07200503@gmail.com>
2023-04-25 10:53:41 +0900

    ♻ バージョンアップによるリネーム

--- src/config/components/model/form/index.tsx
+++ src/config/components/model/form/index.tsx
@@ -4,20 +4,20 @@ import { useRecoilValue } from 'recoil';
 import { storageState } from '../../../states/plugin';
 import Condition from './condition';
 import { ConditionIndexProvider } from '../../condition-index-provider';
-import { PluginContentLayout } from '@konomi-app/kintone-utility-component';
+import { PluginContent } from '@konomi-app/kintone-utility-component';
 
 type Props = Readonly<{
   conditionLength: number;
 }>;
 
 const Component: FCX<Props> = ({ conditionLength }) => (
-  <PluginContentLayout>
+  <PluginContent>
     {new Array(conditionLength).fill('').map((_, index) => (
       <ConditionIndexProvider key={index} conditionIndex={index}>
         <Condition key={index} />
       </ConditionIndexProvider>
     ))}
-  </PluginContentLayout>
+  </PluginContent>
 );
 
 const Container: FC = () => {
