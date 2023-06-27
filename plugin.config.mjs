c3a5465422d18c5605329e098ed592aea72e6259
Ribbit <echo07200503@gmail.com>
2023-06-27 10:23:49 +0900

    🎉 2.22.1

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -9,7 +9,7 @@ export default {
   manifest: {
     base: {
       manifest_version: 1,
-      version: '2.22.0',
+      version: '2.22.1',
       type: 'APP',
       name: {
         en: 'faster search plugin',
