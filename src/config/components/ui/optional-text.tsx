0824873da3b0f3e7eacb45a7167eb7ec0f2625ec
Ribbit <echo07200503@gmail.com>
2023-09-22 16:33:48 +0900

    🎨 微調整

--- src/config/components/ui/optional-text.tsx
+++ src/config/components/ui/optional-text.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 
 export const OptionalText = styled.span`
-  font-size: 12px;
+  font-size: 11px;
   display: block;
-  padding: 4px 8px;
-  border-radius: 8px;
+  padding: 3px 8px;
+  border-radius: 4px;
 
   border: 1px solid #1d4ed8;
   color: #1d4ed8;
