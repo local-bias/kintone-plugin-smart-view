14619e0dd2e2bbdf4ccd287eee21859891022033
Ribbit <echo07200503@gmail.com>
2023-11-25 23:11:06 +0900

    ♻ Update app configuration and remove unused components

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,6 +1,12 @@
 import { PluginErrorBoundary } from '@/lib/components/error-boundary';
 import { URL_PROMOTION } from '@/lib/statics';
-import { PluginBanner, PluginContent, PluginLayout } from '@konomi-app/kintone-utilities-react';
+import {
+  PluginBanner,
+  PluginContent,
+  PluginLayout,
+  PluginConfigProvider,
+  Notification,
+} from '@konomi-app/kintone-utilities-react';
 import { LoaderWithLabel } from '@konomi-app/ui-react';
 import { SnackbarProvider } from 'notistack';
 import React, { FC, Suspense } from 'react';
@@ -8,27 +14,29 @@ import { RecoilRoot } from 'recoil';
 import Footer from './components/model/footer';
 import Form from './components/model/form';
 import Sidebar from './components/model/sidebar';
-import Announcement from './components/model/announcement';
+import config from '../../plugin.config.mjs';
 
 const Component: FC = () => (
   <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
     <RecoilRoot>
       <PluginErrorBoundary>
-        <Announcement />
-        <SnackbarProvider maxSnack={1}>
-          <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています...' />}>
-            <PluginLayout>
-              <Sidebar />
-              <PluginContent>
-                <PluginErrorBoundary>
-                  <Form />
-                </PluginErrorBoundary>
-              </PluginContent>
-              <PluginBanner url='https://promotion.konomi.app/kintone-plugin/paid' />
-              <Footer />
-            </PluginLayout>
-          </Suspense>
-        </SnackbarProvider>
+        <PluginConfigProvider config={config}>
+          <Notification />
+          <SnackbarProvider maxSnack={1}>
+            <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています...' />}>
+              <PluginLayout>
+                <Sidebar />
+                <PluginContent>
+                  <PluginErrorBoundary>
+                    <Form />
+                  </PluginErrorBoundary>
+                </PluginContent>
+                <PluginBanner url='https://promotion.konomi.app/kintone-plugin/paid' />
+                <Footer />
+              </PluginLayout>
+            </Suspense>
+          </SnackbarProvider>
+        </PluginConfigProvider>
       </PluginErrorBoundary>
     </RecoilRoot>
     <iframe
