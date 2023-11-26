4d3eef9ace6aa92bf54a8ec3dded70bdfa9992e9
Ribbit <echo07200503@gmail.com>
2023-11-26 11:39:35 +0900

    🔥 Remove unused constants and update URLs in statics.ts

--- src/lib/statics.ts
+++ src/lib/statics.ts
@@ -1,16 +1,11 @@
 import manifest from '../../plugin/manifest.json';
 
-export const PLUGIN_KEY = 'smart-view';
 export const PLUGIN_NAME = manifest.name.ja;
 export const PLUGIN_VERSION = manifest.version;
-
-export const LOCAL_STORAGE_PREFIX = 'ribbit-kintone-plugin-';
-export const LOCAL_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}${PLUGIN_KEY}`;
 
 export const URL_HOMEPAGE = 'https://ribbit.konomi.app';
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
-export const URL_PLUGIN_LIST = 'https://ribbit.konomi.app/kintone-plugin/';
 
 export const VIEW_ROOT_ID = 'ribbit-smart-view-root';
 
