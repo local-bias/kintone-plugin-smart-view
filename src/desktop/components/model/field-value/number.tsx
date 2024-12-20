998c6eedd61abdb639057dbd21a6e3b506ec698f
Ribbit <echo07200503@gmail.com>
2024-12-21 08:20:53 +0900

    ✨ RecoilからJotaiへの状態管理の移行に伴い、NumberFieldValueコンポーネントをリファクタリングし、数値フィールドの値を取得する処理を簡素化

--- src/desktop/components/model/field-value/number.tsx
+++ src/desktop/components/model/field-value/number.tsx
@@ -1,50 +1,32 @@
-import { Skeleton } from '@mui/material';
-import { FC, Suspense } from 'react';
-import { useRecoilValue } from 'recoil';
-import type { kintoneAPI } from '@konomi-app/kintone-utilities';
-import { appPropertiesState } from '../../../states/kintone';
 import { LANGUAGE } from '@/lib/global';
+import { getNumberFieldValueAsString, type kintoneAPI } from '@konomi-app/kintone-utilities';
+import { Skeleton } from '@mui/material';
+import { useAtomValue } from 'jotai';
+import { FC, Suspense } from 'react';
+import { appPropertiesAtom } from '../../../states/kintone';
 
 type Props = { field: kintoneAPI.field.Number; code: string };
 
-const Component: FC<Props> = ({ field, code }) => {
-  const properties = useRecoilValue(appPropertiesState);
-  const found = Object.entries(properties).find(([key]) => code === key);
+const NumberFieldValue: FC<Props> = ({ field, code }) => {
+  const properties = useAtomValue(appPropertiesAtom);
+  const found = Object.entries(properties).find(([key]) => code === key) as
+    | [string, kintoneAPI.property.Number]
+    | undefined;
 
-  if (!found || ['', undefined, null].includes(field.value) || isNaN(Number(field.value))) {
+  if (!found || ['', undefined, null].includes(field.value)) {
     return <>{field.value}</>;
   }
   const property = found[1] as kintoneAPI.property.Number;
 
-  const casted = Number(field.value);
-  const scaled = property?.displayScale
-    ? Math.round(casted * Math.pow(10, Number(property.displayScale))) /
-      Math.pow(10, Number(property.displayScale))
-    : casted;
-  const separated = property?.digit
-    ? Number(scaled).toLocaleString(LANGUAGE, {
-        maximumFractionDigits: Number(property?.displayScale ?? 0),
-        minimumFractionDigits: Number(property?.displayScale ?? 0),
-      })
-    : Number(scaled).toFixed(Number(property?.displayScale ?? 0));
-
-  if (property?.unit) {
-    if (property.unitPosition === 'BEFORE') {
-      return <>{`${property.unit}${separated}`}</>;
-    } else {
-      return <>{`${separated}${property.unit}`}</>;
-    }
-  }
-
-  return <>{separated}</>;
+  return <>{getNumberFieldValueAsString({ field, property, locales: LANGUAGE })}</>;
 };
 
-const Container: FC<Props> = (props) => {
+const NumberFieldValueContainer: FC<Props> = (props) => {
   return (
     <Suspense fallback={<Skeleton variant='text' />}>
-      <Component {...props} />
+      <NumberFieldValue {...props} />
     </Suspense>
   );
 };
 
-export default Container;
+export default NumberFieldValueContainer;
