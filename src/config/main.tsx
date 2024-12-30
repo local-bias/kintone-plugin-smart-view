6506999edacf06856b59da24fd5192b67cc824ab
Ribbit <echo07200503@gmail.com>
2024-12-30 10:23:19 +0900

    🌐 i18n

--- src/config/main.tsx
+++ src/config/main.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
+import { t } from '@/lib/i18n';
 import { createRoot } from 'react-dom/client';
-
 import App from './app';
 
 const root = document.getElementById('settings');
 if (!root) {
-  throw new Error(
-    'プラグインのHTMLに、ルート要素が存在しません。プラグイン設定をレンダリングするためには、id="settings"の要素が必要です。'
-  );
+  throw new Error(t('config.error.root-not-found'));
 }
 createRoot(root).render(<App />);
