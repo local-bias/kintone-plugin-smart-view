28daf45d83d9a2645b2cc86c86934dacfcaabcb8
Ribbit <echo07200503@gmail.com>
2024-12-31 09:38:21 +0900

    ♻ recoil ➡ jotai

--- src/config/components/model/form/form-join-condition/index.tsx
+++ src/config/components/model/form/form-join-condition/index.tsx
@@ -1,6 +1,14 @@
-import React, { FC } from 'react';
+import { joinConditionsAtom } from '@/config/states/plugin';
+import { JotaiDndContext, JotaiSortableContext } from '@konomi-app/kintone-utilities-jotai';
+import { FC } from 'react';
 import Form from './form';
 
-const Component: FC = () => <Form />;
+const Component: FC = () => (
+  <JotaiDndContext atom={joinConditionsAtom}>
+    <JotaiSortableContext atom={joinConditionsAtom}>
+      <Form />
+    </JotaiSortableContext>
+  </JotaiDndContext>
+);
 
 export default Component;
