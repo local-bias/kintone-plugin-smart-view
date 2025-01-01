bdb452c43be864495379f6a957bbb0f3e5d7bae9
Ribbit <echo07200503@gmail.com>
2025-01-01 10:27:53 +0900

    ♻ recoil ➡ jotai

--- src/config/components/model/footer/index.tsx
+++ src/config/components/model/footer/index.tsx
@@ -6,10 +6,11 @@ import SaveIcon from '@mui/icons-material/Save';
 import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
 import { Button, CircularProgress } from '@mui/material';
 import { produce } from 'immer';
+import { useAtomValue } from 'jotai';
+import { useAtomCallback } from 'jotai/utils';
 import { useSnackbar } from 'notistack';
-import React, { FC, useCallback } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { loadingState, storageState } from '../../../states/plugin';
+import { FC, useCallback } from 'react';
+import { loadingAtom, loadingCountAtom, pluginConfigAtom } from '../../../states/plugin';
 import ExportButton from './export-button';
 import ImportButton from './import-button';
 import ResetButton from './reset-button';
@@ -20,7 +21,7 @@ type Props = {
 };
 
 const Component: FC<Props> = ({ onSaveButtonClick, onBackButtonClick }) => {
-  const loading = useRecoilValue(loadingState);
+  const loading = useAtomValue(loadingAtom);
 
   return (
     <PluginFooter className='[&_button]:m-2'>
@@ -60,76 +61,74 @@ const Container: FC = () => {
 
   const onBackButtonClick = useCallback(() => history.back(), []);
 
-  const onSaveButtonClick = useRecoilCallback(
-    ({ set, snapshot }) =>
-      async () => {
-        try {
-          set(loadingState, true);
-          const storage = await snapshot.getPromise(storageState);
+  const onSaveButtonClick = useAtomCallback(
+    useCallback(async (get, set) => {
+      try {
+        set(loadingCountAtom, (c) => c + 1);
+        const storage = await get(pluginConfigAtom);
 
-          const app = getAppId();
-          if (!app) {
-            throw new Error('アプリのフィールド情報が取得できませんでした');
+        const app = getAppId();
+        if (!app) {
+          throw new Error('アプリのフィールド情報が取得できませんでした');
+        }
+        const { views } = await getViews({
+          app,
+          preview: true,
+          guestSpaceId: GUEST_SPACE_ID,
+          debug: process.env.NODE_ENV === 'development',
+        });
+
+        const newViews = produce(views, (draft) => {
+          for (const condition of storage?.conditions || []) {
+            for (const view of Object.values(draft)) {
+              if (view.id === condition.viewId && view.type === 'CUSTOM') {
+                view.html = `<div id='${VIEW_ROOT_ID}'></div>`;
+                view.pager = false;
+              }
+            }
           }
-          const { views } = await getViews({
+        });
+
+        let warning: string = '';
+        try {
+          await updateViews({
             app,
-            preview: true,
+            views: newViews,
             guestSpaceId: GUEST_SPACE_ID,
             debug: process.env.NODE_ENV === 'development',
           });
-
-          const newViews = produce(views, (draft) => {
-            for (const condition of storage?.conditions || []) {
-              for (const view of Object.values(draft)) {
-                if (view.id === condition.viewId && view.type === 'CUSTOM') {
-                  view.html = `<div id='${VIEW_ROOT_ID}'></div>`;
-                  view.pager = false;
-                }
-              }
-            }
-          });
-
-          let warning: string = '';
-          try {
-            await updateViews({
-              app,
-              views: newViews,
-              guestSpaceId: GUEST_SPACE_ID,
-              debug: process.env.NODE_ENV === 'development',
-            });
-          } catch (error: any) {
-            console.error(error);
-            if (error?.code === 'CB_NO02') {
-              warning =
-                '設定を更新しましたが、システム管理権限がないため、一覧の更新がスキップされました。';
-            }
+        } catch (error: any) {
+          console.error(error);
+          if (error?.code === 'CB_NO02') {
+            warning =
+              '設定を更新しましたが、システム管理権限がないため、一覧の更新がスキップされました。';
           }
-
-          storeStorage(storage!, () => true);
-          if (warning) {
-            enqueueSnackbar(warning, {
-              variant: 'warning',
-              action: (
-                <Button color='inherit' size='small' variant='outlined' onClick={onBackButtonClick}>
-                  プラグイン一覧に戻る
-                </Button>
-              ),
-            });
-          } else {
-            enqueueSnackbar('設定を保存しました', {
-              variant: 'success',
-              action: (
-                <Button color='inherit' size='small' variant='outlined' onClick={onBackButtonClick}>
-                  プラグイン一覧に戻る
-                </Button>
-              ),
-            });
-          }
-        } finally {
-          set(loadingState, false);
         }
-      },
-    []
+
+        storeStorage(storage!, () => true);
+        if (warning) {
+          enqueueSnackbar(warning, {
+            variant: 'warning',
+            action: (
+              <Button color='inherit' size='small' variant='outlined' onClick={onBackButtonClick}>
+                プラグイン一覧に戻る
+              </Button>
+            ),
+          });
+        } else {
+          enqueueSnackbar('設定を保存しました', {
+            variant: 'success',
+            action: (
+              <Button color='inherit' size='small' variant='outlined' onClick={onBackButtonClick}>
+                プラグイン一覧に戻る
+              </Button>
+            ),
+          });
+        }
+      } finally {
+        set(loadingCountAtom, (c) => c - 1);
+      }
+    }, [])
   );
 
   return <Component {...{ onSaveButtonClick, onBackButtonClick }} />;
