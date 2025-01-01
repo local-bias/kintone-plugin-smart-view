5d901c3f6f7bc1b231fb217e1887fed4027808ef
Ribbit <echo07200503@gmail.com>
2025-01-01 10:30:19 +0900

    ♻ LayoutコンポーネントにpluginConditionを追加

--- src/desktop/components/model/header/index.tsx
+++ src/desktop/components/model/header/index.tsx
@@ -7,20 +7,26 @@ import Pagination from './pagination';
 import PaginationChunk from './pagination-chunk';
 import ExtractedInputs from './extracted-inputs';
 import ViewTypeSelect from './view-type-select';
+import { useAtomValue } from 'jotai';
+import { pluginConditionAtom } from '@/desktop/states/plugin';
 
-const Component: FC = () => (
-  <Layout>
-    <div className='flex flex-wrap gap-4 items-center'>
-      <SearchInput />
-      <ExtractedInputs />
-    </div>
-    <div>
-      <CSVDownloadButton />
-      <ViewTypeSelect />
-      <Pagination />
-      <PaginationChunk />
-    </div>
-  </Layout>
-);
+const Component: FC = () => {
+  const condition = useAtomValue(pluginConditionAtom)!;
+
+  return (
+    <Layout condition={condition}>
+      <div className='flex flex-wrap gap-4 items-center'>
+        <SearchInput />
+        <ExtractedInputs />
+      </div>
+      <div>
+        <CSVDownloadButton />
+        <ViewTypeSelect />
+        <Pagination />
+        <PaginationChunk />
+      </div>
+    </Layout>
+  );
+};
 
 export default Component;
