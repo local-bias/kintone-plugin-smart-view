de86e216e49c1b55d96eaf01a0cd843aa6a5ea1e
Ribbit <echo07200503@gmail.com>
2025-03-02 20:30:37 +0900

    ⚡ アプリ設定情報をコンポーネント外で非同期的に取得

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,21 +1,23 @@
 import { PluginErrorBoundary } from '@/lib/components/error-boundary';
+import { ThemeProvider } from '@/lib/components/theme-provider';
+import { t } from '@/lib/i18n';
 import { URL_PROMOTION } from '@/lib/statics';
+import { store } from '@/lib/store';
 import {
+  Notification,
   PluginBanner,
+  PluginConfigProvider,
   PluginContent,
   PluginLayout,
-  PluginConfigProvider,
-  Notification,
 } from '@konomi-app/kintone-utilities-react';
 import { LoaderWithLabel } from '@konomi-app/ui-react';
+import { Provider } from 'jotai';
 import { SnackbarProvider } from 'notistack';
 import { FC, Suspense } from 'react';
+import config from '../../plugin.config.mjs';
 import Footer from './components/model/footer';
 import Form from './components/model/form';
 import Sidebar from './components/sidebar';
-import config from '../../plugin.config.mjs';
-import { ThemeProvider } from '@/lib/components/theme-provider';
-import { t } from '@/lib/i18n';
 
 const Component: FC = () => {
   return (
@@ -33,23 +35,30 @@ const Component: FC = () => {
 };
 
 const Container: FC = () => (
-  <Suspense fallback={<LoaderWithLabel label={t('config.app.root.loading')} />}>
-    <ThemeProvider>
-      <PluginErrorBoundary>
-        <PluginConfigProvider config={config}>
-          <Notification />
-          <SnackbarProvider maxSnack={1}>
-            <Suspense fallback={<LoaderWithLabel label={t('config.app.config.loading')} />}>
-              <PluginLayout>
-                <Component />
-              </PluginLayout>
-            </Suspense>
-          </SnackbarProvider>
-        </PluginConfigProvider>
-      </PluginErrorBoundary>
-    </ThemeProvider>
-    <iframe title='promotion' loading='lazy' src={URL_PROMOTION} className='border-0 w-full h-16' />
-  </Suspense>
+  <Provider store={store}>
+    <Suspense fallback={<LoaderWithLabel label={t('config.app.root.loading')} />}>
+      <ThemeProvider>
+        <PluginErrorBoundary>
+          <PluginConfigProvider config={config}>
+            <Notification />
+            <SnackbarProvider maxSnack={1}>
+              <Suspense fallback={<LoaderWithLabel label={t('config.app.config.loading')} />}>
+                <PluginLayout>
+                  <Component />
+                </PluginLayout>
+              </Suspense>
+            </SnackbarProvider>
+          </PluginConfigProvider>
+        </PluginErrorBoundary>
+      </ThemeProvider>
+      <iframe
+        title='promotion'
+        loading='lazy'
+        src={URL_PROMOTION}
+        className='border-0 w-full h-16'
+      />
+    </Suspense>
+  </Provider>
 );
 
 export default Container;
