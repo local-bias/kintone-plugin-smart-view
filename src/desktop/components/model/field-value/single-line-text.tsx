9a2c67f8452e8016daf8b2519c76711f4386634b
Ribbit <echo07200503@gmail.com>
2025-04-05 12:02:32 +0900

    ♻ より適切なコンポーネント名に変更し、import順を整理

--- src/desktop/components/model/field-value/single-line-text.tsx
+++ src/desktop/components/model/field-value/single-line-text.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
 import type { kintoneAPI } from '@konomi-app/kintone-utilities';
+import { FC } from 'react';
 
 type Props = { field: kintoneAPI.field.SingleLineText };
 
-const Component: FC<Props> = (props) => {
+const SingleLineTextFieldValue: FC<Props> = (props) => {
   return <>{props.field.value}</>;
 };
 
-export default Component;
+export default SingleLineTextFieldValue;
