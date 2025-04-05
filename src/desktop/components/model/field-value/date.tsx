314fef7adc3226d44d87cc0f1fc89e96577e17d7
Ribbit <echo07200503@gmail.com>
2025-04-05 12:01:58 +0900

    ♻ より適切なコンポーネント名に変更し、import順を整理

--- src/desktop/components/model/field-value/date.tsx
+++ src/desktop/components/model/field-value/date.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
-import type { DeepReadonly } from 'utility-types';
 import type { kintoneAPI } from '@konomi-app/kintone-utilities';
+import { FC } from 'react';
+import type { DeepReadonly } from 'utility-types';
 
 type Props = DeepReadonly<{ field: kintoneAPI.field.Date }>;
 
-const Component: FC<Props> = ({ field }) => {
+const DateFieldValue: FC<Props> = ({ field }) => {
   return <>{field.value ? new Date(field.value).toLocaleDateString() : ''}</>;
 };
 
-export default Component;
+export default DateFieldValue;
