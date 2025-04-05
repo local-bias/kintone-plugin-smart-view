836fc9910e8cf95030d4741f160d126a746d5301
Ribbit <echo07200503@gmail.com>
2025-04-05 12:02:36 +0900

    ♻ より適切なコンポーネント名に変更し、import順を整理

--- src/desktop/components/model/field-value/user.tsx
+++ src/desktop/components/model/field-value/user.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react';
-import type { DeepReadonly } from 'utility-types';
 import type { kintoneAPI } from '@konomi-app/kintone-utilities';
+import { FC } from 'react';
+import type { DeepReadonly } from 'utility-types';
 
 type Props = DeepReadonly<{ field: kintoneAPI.field.Creator | kintoneAPI.field.Modifier }>;
 
-const Component: FC<Props> = ({ field }) => {
+const UserFieldValue: FC<Props> = ({ field }) => {
   return <>{field.value.name}</>;
 };
 
-export default Component;
+export default UserFieldValue;
