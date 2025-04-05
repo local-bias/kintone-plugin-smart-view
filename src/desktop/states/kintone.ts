961c3c9028e094b45d7fe8eaf142b53c0bb91c73
Ribbit <echo07200503@gmail.com>
2025-04-05 12:08:56 +0900

    ✨ 現在のアプリのフィールドプロパティを取得するatomを追加し、フィールドのプロパティを返すatomFamilyを実装

--- src/desktop/states/kintone.ts
+++ src/desktop/states/kintone.ts
@@ -3,17 +3,48 @@ import { t } from '@/lib/i18n';
 import { downloadFile, getAppId, kintoneAPI } from '@konomi-app/kintone-utilities';
 import { atom } from 'jotai';
 import { atomFamily } from 'jotai/utils';
+import { resolvedTableColumnsAtom } from './plugin';
 
 export const currentAppIdAtom = atom(() => {
   const appId = getAppId();
   if (!appId) {
     throw new Error(t('desktop.error.appInfoRetrievalFailedError'));
   }
-  return appId;
+  return String(appId);
 });
 
-export const appPropertiesAtom = atom<kintoneAPI.FieldProperties>({});
+export const fieldPropertiesAtom = atomFamily((_appId: string) =>
+  atom<kintoneAPI.FieldProperties>({})
+);
+
+/**
+ * 現在のアプリのフィールドプロパティを取得するatom
+ *
+ * 🚧 マイグレーションにあたり、暫定的にこのatomを参照している可能性があります
+ */
+export const currentAppFieldPropertiesAtom = atom((get) => {
+  const appId = get(currentAppIdAtom);
+  const properties = get(fieldPropertiesAtom(appId));
+  return properties;
+});
+
 export const appFormLayoutAtom = atom<kintoneAPI.Layout>([]);
+
+/**
+ * viewFieldのIDを受け取り、そのフィールドのプロパティを返す
+ */
+export const tableColumnFieldPropertyAtom = atomFamily((columnId: string) =>
+  atom<Promise<kintoneAPI.FieldProperty | null>>(async (get) => {
+    const columns = get(resolvedTableColumnsAtom);
+    const column = columns.find((v) => v.id === columnId);
+    if (!column) {
+      return null;
+    }
+    const appProperties = get(fieldPropertiesAtom(column.appId));
+    const property = appProperties[column.fieldCode];
+    return property ?? null;
+  })
+);
 
 export const propertiesReadyAtom = atom(false);
 export const formLayoutReadyAtom = atom(false);
