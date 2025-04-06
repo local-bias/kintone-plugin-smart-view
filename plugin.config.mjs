1fa041fdc1793f1041ac943a9f05cf9b43476b3b
Ribbit <echo07200503@gmail.com>
2025-04-06 14:54:47 +0900

    🔖 プラグインのバージョンを4.2.1から5.1.0に更新

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -24,7 +24,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '4.2.1',
+      version: '5.1.0',
       type: 'APP',
       name: {
         en: 'faster search plugin',
