2888f93c59f1b7903a96708a45e81e3112a3812d
adachi <echo07200503@gmail.com>
2025-08-03 06:59:40 +0900

    ♻ バージョンを5.1.0から5.3.0に更新

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -24,17 +24,26 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '5.1.0',
+      version: '5.3.0',
       type: 'APP',
       name: {
         en: 'faster search plugin',
         ja: '一覧高速検索＆絞り込みプラグイン',
         zh: '更快的搜索插件',
+        'zh-TW': '更快的搜尋插件',
+        es: 'Complemento de búsqueda rápida',
+        'pt-BR': 'Plugin de busca rápida',
+        th: 'ปลั๊กอินค้นหาอย่างรวดเร็ว',
       },
       description: {
         en: 'This plugin replaces the default view with a customized view and enables faster searches',
         ja: '指定した一覧を独自のテーブルに置き換え、高速な検索と絞り込みを実現します。',
         zh: '此插件用自定义视图替换默认视图并实现更快的搜索',
+        'zh-TW': '此插件透過無縫連接多個應用程式來大幅提高工作效率，除了快速搜尋。',
+        es: 'Este complemento mejora enormemente la eficiencia del trabajo al conectar sin problemas múltiples aplicaciones además de la búsqueda rápida.',
+        'pt-BR':
+          'Este plugin melhora muito a eficiência do trabalho conectando vários aplicativos de forma contínua, além de uma busca rápida.',
+        th: 'ปลั๊กอินนี้ช่วยเพิ่มประสิทธิภาพการทำงานอย่างมากโดยการเชื่อมต่อแอปพลิเคชันหลายตัวอย่างไร้รอยต่อ นอกเหนือจากการค้นหาอย่างรวดเร็ว',
       },
       icon: 'icon.png',
       homepage_url: { ja: HP, en: HP },
