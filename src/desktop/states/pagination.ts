e9ce498fcd1c6aaa67d96ca3dfaa6a298f59a27e
adachi <echo07200503@gmail.com>
2025-08-03 17:12:38 +0900

    ✨ handlePaginationIndexChangeAtomを追加し、ページネーションインデックスの変更を管理するロジックを実装

--- src/desktop/states/pagination.ts
+++ src/desktop/states/pagination.ts
@@ -2,4 +2,8 @@ import { atom } from 'jotai';
 
 export const paginationIndexAtom = atom(1);
 
+export const handlePaginationIndexChangeAtom = atom(null, (_, set, __: unknown, index: number) => {
+  set(paginationIndexAtom, index);
+});
+
 export const paginationChunkAtom = atom(100);
