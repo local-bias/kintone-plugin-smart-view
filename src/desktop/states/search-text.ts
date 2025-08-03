ea177c2b4b6bca547ed61f79cda04f6bbc639f87
adachi <echo07200503@gmail.com>
2025-08-03 17:12:27 +0900

    ✨ handleSearchInputChangeAtomを追加し、検索テキストの変更を管理するロジックを実装

--- src/desktop/states/search-text.ts
+++ src/desktop/states/search-text.ts
@@ -1,12 +1,23 @@
 import { URL_SEARCH_PARAMS_TEXT } from '@/lib/statics';
+import { store } from '@/lib/store';
 import { atom } from 'jotai';
-import { atomEffect } from 'jotai-effect';
+import { observe } from 'jotai-effect';
+import { paginationIndexAtom } from './pagination';
+import { ChangeEvent } from 'react';
 
 export const searchTextAtom = atom(
   new URLSearchParams(location.search).get(URL_SEARCH_PARAMS_TEXT) ?? ''
 );
 
-export const searchTextEffect = atomEffect((get) => {
+export const handleSearchInputChangeAtom = atom(
+  null,
+  (_, set, e: ChangeEvent<HTMLInputElement>) => {
+    set(searchTextAtom, e.target.value);
+    set(paginationIndexAtom, 1);
+  }
+);
+
+observe((get) => {
   const text = get(searchTextAtom);
   const url = new URL(location.href);
   if (!text) {
@@ -15,4 +26,4 @@ export const searchTextEffect = atomEffect((get) => {
     url.searchParams.set(URL_SEARCH_PARAMS_TEXT, text);
   }
   history.replaceState(null, '', url.toString());
-});
+}, store);
