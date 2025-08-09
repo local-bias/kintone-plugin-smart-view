2d7bc62dd25c1b1e545cb740f06cd0e2d9999ad1
adachi <echo07200503@gmail.com>
2025-08-09 21:40:47 +0900

    ✨ 新しいチャートデータ作成関数を追加し、データの集計と色付けを実装

---
+++ src/lib/chart.ts
@@ -0,0 +1,67 @@
+import { ResolvedTableColumnProps } from '@/desktop/states/plugin';
+import { getFieldValueAsString, kintoneAPI } from '@konomi-app/kintone-utilities';
+
+export const CHART_COLORS = [
+  'var(--🐸chart-1)',
+  'var(--🐸chart-2)',
+  'var(--🐸chart-3)',
+  'var(--🐸chart-4)',
+  'var(--🐸chart-5)',
+  'var(--🐸chart-6)',
+  'var(--🐸chart-7)',
+];
+
+export function createChartData(params: {
+  col: ResolvedTableColumnProps;
+  field: kintoneAPI.field.Subtable;
+}): {
+  total: number;
+  max: number;
+  data: {
+    label: string;
+    value: number;
+    color: string;
+  }[];
+} {
+  const { col, field } = params;
+
+  function getAmount(valueField?: kintoneAPI.Field): number {
+    if (!valueField || !['NUMBER', 'CALC'].includes(valueField.type)) {
+      return 0;
+    }
+    const amount = Number(valueField.value);
+    return isNaN(amount) ? 0 : amount;
+  }
+
+  let total = 0;
+  let max = 0;
+  const data: Record<string, { value: number }> = {};
+  field.value.forEach((row) => {
+    const amount = getAmount(row.value[col.miniGraphValueFieldCode ?? '']);
+    const labelField = row.value[col.miniGraphLabelFieldCode ?? ''];
+    const labelPlaceholder = `No Label`;
+    const label = labelField
+      ? getFieldValueAsString(labelField) || labelPlaceholder
+      : labelPlaceholder;
+    max = Math.max(max, amount);
+    total += amount;
+    if (data[label]) {
+      data[label].value += amount;
+    } else {
+      data[label] = { value: amount };
+    }
+  });
+
+  // 配列に直し、大きい順にソート
+  return {
+    total,
+    max,
+    data: Object.entries(data)
+      .map(([label, { value }]) => ({ label, value }))
+      .sort((a, b) => b.value - a.value)
+      .map((item, i) => ({
+        ...item,
+        color: CHART_COLORS[i % CHART_COLORS.length],
+      })),
+  };
+}
