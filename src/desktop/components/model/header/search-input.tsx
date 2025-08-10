61dd99c35dfebf245717b22414364df52f74ca8e
adachi <echo07200503@gmail.com>
2025-08-10 19:02:03 +0900

    ♻ SearchInputコンポーネントのリファクタリング: 不要なコードを削除し、状態管理を簡素化

--- src/desktop/components/model/header/search-input.tsx
+++ src/desktop/components/model/header/search-input.tsx
@@ -1,45 +1,31 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { InputAdornment, TextField } from '@mui/material';
-import { useAtomValue } from 'jotai';
-import { useAtomCallback } from 'jotai/utils';
-import { ChangeEvent, ChangeEventHandler, FC, FCX, useCallback } from 'react';
-import type { DeepReadonly } from 'utility-types';
-import { paginationIndexAtom } from '../../../states/pagination';
-import { searchTextAtom } from '../../../states/search-text';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { FC } from 'react';
+import { handleSearchInputChangeAtom, searchTextAtom } from '../../../states/search-text';
 
-type Props = DeepReadonly<{
-  searchText: string;
-  onSearchTextChange: ChangeEventHandler<HTMLInputElement>;
-}>;
-
-const Component: FCX<Props> = ({ searchText, onSearchTextChange }) => (
-  <TextField
-    variant='outlined'
-    color='primary'
-    label='フィールドを検索'
-    value={searchText}
-    onChange={onSearchTextChange}
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position='start'>
-          <SearchIcon />
-        </InputAdornment>
-      ),
-    }}
-  />
-);
-
-const Container: FC = () => {
+const SearchInput: FC = () => {
   const searchText = useAtomValue(searchTextAtom);
+  const onSearchTextChange = useSetAtom(handleSearchInputChangeAtom);
 
-  const onSearchTextChange = useAtomCallback(
-    useCallback((get, set, e: ChangeEvent<HTMLInputElement>) => {
-      set(searchTextAtom, e.target.value);
-      set(paginationIndexAtom, 1);
-    }, [])
+  return (
+    <TextField
+      variant='outlined'
+      color='primary'
+      label='フィールドを検索'
+      value={searchText}
+      onChange={onSearchTextChange}
+      slotProps={{
+        input: {
+          startAdornment: (
+            <InputAdornment position='start'>
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        },
+      }}
+    />
   );
-
-  return <Component {...{ searchText, onSearchTextChange }} />;
 };
 
-export default Container;
+export default SearchInput;
