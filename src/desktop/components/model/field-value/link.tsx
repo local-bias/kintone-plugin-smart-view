f883665022a0f99db028e3f9fe5940f8fb9facbd
adachi <echo07200503@gmail.com>
2025-08-31 08:18:19 +0900

    ♻ LinkFieldValueコンポーネントの修正: appIdプロパティを追加し、プロパティの取得方法を変更

--- src/desktop/components/model/field-value/link.tsx
+++ src/desktop/components/model/field-value/link.tsx
@@ -1,19 +1,22 @@
-import { currentAppFieldPropertiesAtom } from '@/desktop/states/kintone';
+import { appFormPropertyAtom } from '@/desktop/states/kintone';
 import { type kintoneAPI } from '@konomi-app/kintone-utilities';
-import { Skeleton } from '@mui/material';
 import { useAtomValue } from 'jotai';
-import { FC, Suspense } from 'react';
+import { FC } from 'react';
 
-type Props = { field: kintoneAPI.field.Link; code: string };
+type Props = {
+  field: kintoneAPI.field.Link;
+  code: string;
+  appId: string;
+};
 
-const Component: FC<Props> = ({ field, code }) => {
-  const properties = useAtomValue(currentAppFieldPropertiesAtom);
-  const found = Object.entries(properties).find(([key]) => code === key);
+const LinkFieldValue: FC<Props> = ({ field, code, appId }) => {
+  const property = useAtomValue(
+    appFormPropertyAtom({ appId, fieldCode: code })
+  ) as kintoneAPI.property.Link | null;
 
-  if (!found || ['', undefined, null].includes(field.value)) {
+  if (!property || ['', undefined, null].includes(field.value)) {
     return <>{field.value}</>;
   }
-  const property = found[1] as kintoneAPI.property.Link;
 
   switch (property.protocol) {
     case 'WEB':
@@ -31,12 +34,4 @@ const Component: FC<Props> = ({ field, code }) => {
   }
 };
 
-const Container: FC<Props> = (props) => {
-  return (
-    <Suspense fallback={<Skeleton variant='text' />}>
-      <Component {...props} />
-    </Suspense>
-  );
-};
-
-export default Container;
+export default LinkFieldValue;
