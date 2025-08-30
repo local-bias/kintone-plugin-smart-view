4afbc9892cbf566bca9b5456df6152de0bf1d0d4
adachi <echo07200503@gmail.com>
2025-08-31 08:17:55 +0900

    ♻ CalcFieldValueコンポーネントのリファクタリング: 不要なコードを削除し、プロパティの取得方法を修正

--- src/desktop/components/model/field-value/calc.tsx
+++ src/desktop/components/model/field-value/calc.tsx
@@ -1,29 +1,20 @@
 import { getCalcFieldValueAsString, type kintoneAPI } from '@konomi-app/kintone-utilities';
-import { Skeleton } from '@mui/material';
 import { useAtomValue } from 'jotai';
-import { FC, Suspense } from 'react';
-import { fieldPropertiesAtom } from '../../../states/kintone';
+import { FC } from 'react';
+import { appFormPropertyAtom } from '../../../states/kintone';
 
 type Props = { field: kintoneAPI.field.Calc; code: string; appId: string };
 
-const Component: FC<Props> = ({ field, code, appId }) => {
-  const properties = useAtomValue(fieldPropertiesAtom(appId));
-  const found = Object.entries(properties).find(([key]) => code === key);
+const CalcFieldValue: FC<Props> = ({ field, code, appId }) => {
+  const property = useAtomValue(
+    appFormPropertyAtom({ appId, fieldCode: code })
+  ) as kintoneAPI.property.Calc | null;
 
-  if (!found || ['', undefined, null].includes(field.value)) {
+  if (!property || ['', undefined, null].includes(field.value)) {
     return <>{field.value}</>;
   }
-  const property = found[1] as kintoneAPI.property.Calc;
 
   return <>{getCalcFieldValueAsString({ field, property })}</>;
 };
 
-const Container: FC<Props> = (props) => {
-  return (
-    <Suspense fallback={<Skeleton variant='text' />}>
-      <Component {...props} />
-    </Suspense>
-  );
-};
-
-export default Container;
+export default CalcFieldValue;
