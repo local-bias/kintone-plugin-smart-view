66abf407486fc26707443fb8d8c40b19d967bcc5
adachi <echo07200503@gmail.com>
2025-08-31 08:18:46 +0900

    ♻ ExtractedInputsコンポーネントの修正: appPropertiesAtomをcurrentAppFieldPropertiesAtomに変更し、onChangeハンドラーを簡素化

--- src/desktop/components/model/header/extracted-inputs.tsx
+++ src/desktop/components/model/header/extracted-inputs.tsx
@@ -1,26 +1,23 @@
-import { appPropertiesAtom } from '@/desktop/states/kintone';
-import { extractedSearchConditionsAtom, pluginConditionAtom } from '@/desktop/states/plugin';
+import { currentAppFieldPropertiesAtom } from '@/desktop/states/kintone';
+import {
+  extractedSearchConditionsAtom,
+  handleExtractedSearchAutocompleteChangeAtom,
+  handleExtractedSearchInputChangeAtom,
+  pluginConditionAtom,
+} from '@/desktop/states/plugin';
 import { autocompleteValuesAtom } from '@/desktop/states/records';
+import { PluginExtractedInput } from '@/schema/plugin-config';
 import { Autocomplete, Skeleton, TextField, Tooltip } from '@mui/material';
-import { useAtomValue } from 'jotai';
-import { useAtomCallback } from 'jotai/utils';
-import { ChangeEventHandler, FC, Suspense, useCallback } from 'react';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { FC, Suspense } from 'react';
 
-type Props = { input: Plugin.ExtractedInput; index: number };
+type Props = { input: PluginExtractedInput; index: number };
 
 const ExtractedAutocomplete: FC<Props> = ({ input, index }) => {
-  const appFields = useAtomValue(appPropertiesAtom);
+  const appFields = useAtomValue(currentAppFieldPropertiesAtom);
   const searchCondition = useAtomValue(extractedSearchConditionsAtom(index))!;
   const suggests = useAtomValue(autocompleteValuesAtom(input.fieldCode));
-
-  const onChange = useAtomCallback(
-    useCallback(
-      (get, set, value: string | null) => {
-        set(extractedSearchConditionsAtom(index), (_c, c = _c!) => ({ ...c, value: value ?? '' }));
-      },
-      [index]
-    )
-  );
+  const onChange = useSetAtom(handleExtractedSearchAutocompleteChangeAtom(index));
 
   const field = Object.values(appFields).find((field) => field.code === input.fieldCode);
 
@@ -45,7 +42,7 @@ const ExtractedAutocomplete: FC<Props> = ({ input, index }) => {
       value={searchCondition.value || null}
       sx={{ width: '250px' }}
       options={suggests}
-      onChange={(_, field) => onChange(field)}
+      onChange={onChange}
       renderInput={(params) => (
         <TextField
           {...params}
@@ -63,20 +60,9 @@ const ExtractedAutocomplete: FC<Props> = ({ input, index }) => {
 };
 
 const ExtractedInput: FC<Props> = ({ input, index }) => {
-  const appFields = useAtomValue(appPropertiesAtom);
+  const appFields = useAtomValue(currentAppFieldPropertiesAtom);
   const searchCondition = useAtomValue(extractedSearchConditionsAtom(index))!;
-
-  const onChange: ChangeEventHandler<HTMLInputElement> = useAtomCallback(
-    useCallback(
-      (get, set, event) => {
-        set(extractedSearchConditionsAtom(index), (_c, c = _c!) => ({
-          ...c,
-          value: event.target.value,
-        }));
-      },
-      [index]
-    )
-  );
+  const onChange = useSetAtom(handleExtractedSearchInputChangeAtom(index));
 
   const label =
     Object.values(appFields).find((field) => field.code === input.fieldCode)?.label ??
