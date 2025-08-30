10881f48166ad73a97b3753ad61f0e5efa8a8ba2
adachi <echo07200503@gmail.com>
2025-08-31 08:18:05 +0900

    ♻ CategoryFieldValueコンポーネントのリファクタリング: 不要なインポートを削除し、プロパティの取得方法を簡素化

--- src/desktop/components/model/field-value/category.tsx
+++ src/desktop/components/model/field-value/category.tsx
@@ -1,17 +1,13 @@
 import type { kintoneAPI } from '@konomi-app/kintone-utilities';
-import { FC } from 'react';
-import type { DeepReadonly } from 'utility-types';
 
-type Props = DeepReadonly<{ field: kintoneAPI.field.Category }>;
+type Props = { field: kintoneAPI.field.Category };
 
-const CategoryFieldValue: FC<Props> = (props) => {
+export default function CategoryFieldValue({ field }: Props) {
   return (
     <>
-      {props.field.value.map((value, i) => (
+      {field.value.map((value, i) => (
         <div key={i}>{value}</div>
       ))}
     </>
   );
-};
-
-export default CategoryFieldValue;
+}
