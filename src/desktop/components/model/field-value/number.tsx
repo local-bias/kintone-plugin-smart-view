0ca115c9f624de14ee7717fd957b7cfd374a193f
adachi <echo07200503@gmail.com>
2025-08-31 08:18:34 +0900

    ♻ NumberFieldValueコンポーネントのリファクタリング: 不要なコードを削除し、プロパティの取得方法を修正

--- src/desktop/components/model/field-value/number.tsx
+++ src/desktop/components/model/field-value/number.tsx
@@ -1,32 +1,21 @@
 import { LANGUAGE } from '@/lib/global';
 import { getNumberFieldValueAsString, type kintoneAPI } from '@konomi-app/kintone-utilities';
-import { Skeleton } from '@mui/material';
 import { useAtomValue } from 'jotai';
-import { FC, Suspense } from 'react';
-import { fieldPropertiesAtom } from '../../../states/kintone';
+import { FC } from 'react';
+import { appFormPropertyAtom } from '../../../states/kintone';
 
 type Props = { field: kintoneAPI.field.Number; code: string; appId: string };
 
 const NumberFieldValue: FC<Props> = ({ field, code, appId }) => {
-  const properties = useAtomValue(fieldPropertiesAtom(appId));
-  const found = Object.entries(properties).find(([key]) => code === key) as
-    | [string, kintoneAPI.property.Number]
-    | undefined;
+  const property = useAtomValue(
+    appFormPropertyAtom({ appId, fieldCode: code })
+  ) as kintoneAPI.property.Number | null;
 
-  if (!found || ['', undefined, null].includes(field.value)) {
+  if (!property || ['', undefined, null].includes(field.value)) {
     return <>{field.value}</>;
   }
-  const property = found[1] as kintoneAPI.property.Number;
 
   return <>{getNumberFieldValueAsString({ field, property, locales: LANGUAGE })}</>;
 };
 
-const NumberFieldValueContainer: FC<Props> = (props) => {
-  return (
-    <Suspense fallback={<Skeleton variant='text' />}>
-      <NumberFieldValue {...props} />
-    </Suspense>
-  );
-};
-
-export default NumberFieldValueContainer;
+export default NumberFieldValue;
