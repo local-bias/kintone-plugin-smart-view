dbb00a6d836d84c629c13fd0848421270f420f8a
adachi <echo07200503@gmail.com>
2025-10-11 10:26:32 +0900

    ♻ UserFieldValueコンポーネントの型定義を修正し、不要なインポートを削除

--- src/desktop/components/model/field-value/user.tsx
+++ src/desktop/components/model/field-value/user.tsx
@@ -1,11 +1,7 @@
 import type { kintoneAPI } from '@konomi-app/kintone-utilities';
-import { FC } from 'react';
-import type { DeepReadonly } from 'utility-types';
 
-type Props = DeepReadonly<{ field: kintoneAPI.field.Creator | kintoneAPI.field.Modifier }>;
+type Props = Readonly<{ field: kintoneAPI.field.Creator | kintoneAPI.field.Modifier }>;
 
-const UserFieldValue: FC<Props> = ({ field }) => {
+export default function UserFieldValue({ field }: Props) {
   return <>{field.value.name}</>;
-};
-
-export default UserFieldValue;
+}
