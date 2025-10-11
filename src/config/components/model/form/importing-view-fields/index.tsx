ffaf55aa4828f29cc6743e746d38bdbff2729607
adachi <echo07200503@gmail.com>
2025-10-11 10:26:55 +0900

    ♻ memoを削除し、コンポーネントを関数形式に変更

--- src/config/components/model/form/importing-view-fields/index.tsx
+++ src/config/components/model/form/importing-view-fields/index.tsx
@@ -1,13 +1,11 @@
-import React, { memo, FC } from 'react';
+import Button from "./button";
+import Dialog from "./dialog";
 
-import Button from './button';
-import Dialog from './dialog';
-
-const Component: FC = () => (
-  <>
-    <Button />
-    <Dialog />
-  </>
-);
-
-export default memo(Component);
+export default function ViewFieldsImport() {
+  return (
+    <>
+      <Button />
+      <Dialog />
+    </>
+  );
+}
