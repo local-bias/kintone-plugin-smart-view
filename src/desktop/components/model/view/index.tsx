39c7c33e3937129a6c55b819474c632f13e8fc5b
adachi <echo07200503@gmail.com>
2025-10-25 19:01:55 +0900

    ♻ ErrorGuardとEmptyGuardコンポーネントを追加し、Viewコンポーネントの構造を整理

--- src/desktop/components/model/view/index.tsx
+++ src/desktop/components/model/view/index.tsx
@@ -1,27 +1,44 @@
 import { errorAtom, viewTypeAtom } from '@/desktop/states/plugin';
 import { isOriginalTableShownAtom } from '@/desktop/states/records';
 import { useAtomValue } from 'jotai';
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import TableView from '../table';
 import CardView from '../view-card';
 import EmptyStateView from './empty';
 import ErrorNofitication from './error';
 
-const Component: FC = () => {
-  const viewType = useAtomValue(viewTypeAtom);
+const ErrorGuard: FC<PropsWithChildren> = ({ children }) => {
   const error = useAtomValue(errorAtom);
-  const isOriginalTableShown = useAtomValue(isOriginalTableShownAtom);
-
   if (error) {
     return <ErrorNofitication />;
   }
+  return children;
+};
+
+const EmptyGuard: FC<PropsWithChildren> = ({ children }) => {
+  const isOriginalTableShown = useAtomValue(isOriginalTableShownAtom);
   if (!isOriginalTableShown) {
     return <EmptyStateView />;
   }
+  return children;
+};
+
+const ViewTypeSwitcher: FC = () => {
+  const viewType = useAtomValue(viewTypeAtom);
   if (viewType === 'card') {
     return <CardView />;
   }
   return <TableView />;
 };
 
-export default Component;
+const View: FC = () => {
+  return (
+    <ErrorGuard>
+      <EmptyGuard>
+        <ViewTypeSwitcher />
+      </EmptyGuard>
+    </ErrorGuard>
+  );
+};
+
+export default View;
