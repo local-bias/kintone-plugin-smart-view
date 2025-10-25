cfb7cd5d829cba327ca43426b9b5fe457c90ef81
adachi <echo07200503@gmail.com>
2025-10-25 22:18:05 +0900

    ♻ invariantを使用してエラーハンドリングを改善

--- src/config/main.tsx
+++ src/config/main.tsx
@@ -1,9 +1,8 @@
 import { t } from '@/lib/i18n';
 import { createRoot } from 'react-dom/client';
+import invariant from 'tiny-invariant';
 import App from './app';
 
 const root = document.getElementById('settings');
-if (!root) {
-  throw new Error(t('config.error.root-not-found'));
-}
+invariant(root, t('config.error.root-not-found'));
 createRoot(root).render(<App />);
