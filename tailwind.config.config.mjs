ed2bdb66f2f414c99df4c41724897973f93e2f61
adachi <echo07200503@gmail.com>
2025-10-26 11:45:28 +0900

    ♻ tailwind.configファイルを削除し、設定を整理

--- tailwind.config.config.mjs
+++
@@ -1,8 +0,0 @@
-//@ts-check
-import common from './tailwind.config.common.mjs';
-
-/** @type { import('tailwindcss').Config } */
-export default {
-  ...common,
-  content: ['./src/lib/**/*.{ts,js,jsx,tsx}', './src/config/**/*.{ts,js,jsx,tsx}'],
-};
