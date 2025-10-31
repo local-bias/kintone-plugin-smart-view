cc5a639cdfbecffb29b8ab229b78f5e0ddf5b8f9
adachi <echo07200503@gmail.com>
2025-11-01 07:40:50 +0900

    ♻ pluginConditionAtomに加え、resolvedTableColumnsAtomをインポートし、MyTableコンポーネントに表示フィールドを追加。

--- src/desktop/components/model/table/index.tsx
+++ src/desktop/components/model/table/index.tsx
@@ -1,15 +1,16 @@
 import { useAtomValue } from 'jotai';
 import { FC } from 'react';
-import { pluginConditionAtom } from '../../../states/plugin';
+import { pluginConditionAtom, resolvedTableColumnsAtom } from '../../../states/plugin';
 import Body from './body';
 import Head from './head';
 import { MyTable } from './layout';
 
 const Table: FC = () => {
   const condition = useAtomValue(pluginConditionAtom);
+  const viewFields = useAtomValue(resolvedTableColumnsAtom);
 
   return (
-    <MyTable condition={condition}>
+    <MyTable condition={condition} viewFields={viewFields}>
       <Head />
       <Body />
     </MyTable>
