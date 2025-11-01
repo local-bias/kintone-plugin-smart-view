4115d4de645878e0c8637500e4c22b0ea8f661a5
adachi <echo07200503@gmail.com>
2025-11-01 10:27:34 +0900

    ♻ FieldSettingsButtonをインポートし、ヘッダーコンポーネントに追加

--- src/desktop/components/model/header/index.tsx
+++ src/desktop/components/model/header/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import CSVDownloadButton from './csv-download-button';
 import ExtractedInputs from './extracted-inputs';
+import FieldSettingsButton from './field-settings-button';
 import Layout from './layout';
 import Pagination from './pagination';
 import PaginationChunk from './pagination-chunk';
@@ -11,11 +12,12 @@ import ViewTypeSelect from './view-type-select';
 const Component: FC = () => {
   return (
     <Layout>
-      <div className='flex flex-wrap gap-4 items-center'>
+      <div className='rad:flex rad:flex-wrap rad:gap-4 rad:items-center'>
         <SearchInput />
         <ExtractedInputs />
       </div>
       <div>
+        <FieldSettingsButton />
         <CSVDownloadButton />
         <ViewTypeSelect />
         <Pagination />
